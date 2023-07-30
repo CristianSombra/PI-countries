098a2e6b16c5972abd98b204698d0e173086d37a
CristianSombra <cristiansombra87@gmail.com>
2023-07-29 21:09:24 -0300

    Actualización de módulos: funcionando

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,12 +1,25 @@
 import axios from "axios";
 
 export const GET_COUNTRIES = "GET_COUNTRIES"
+// export const GET_COUNTRY = "GET_COUNTRY"
 
 export const getCountries = () => {
     return async function (dispatch) {
-        const apiData = await axios.get("http://localhost:3001");
-
+        const apiData = await axios.get(
+            "http://localhost:3001"
+        );
     const countries = apiData.data;
     dispatch({type: GET_COUNTRIES, payload: countries})
     }; 
 };
+
+// export const getCountry = (id) => {
+//     return async function (dispatch) {
+//         const apiData = await axios.get(
+//             `http://localhost:3001/countries/${id}`
+//         );
+
+//         const country = apiData.data;
+//         dispatch({type: GET_COUNTRY, payload: country})
+//     };
+// };
--- client/src/views/detail/detail.jsx
+++ client/src/views/detail/detail.jsx
@@ -1,7 +1,11 @@
+
 const Detail = () => {
+
     return(
         <>
             <h1> Esta es la vista de DETAIL</h1>
+
+            <p>Esta vista no se para que se utiliza todavia</p>
         </>
     )
 };
